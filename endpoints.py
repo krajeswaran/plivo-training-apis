a73ef5fb2fd639dc0d1d97cf908d64f6104ec2ec
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2016-06-01 15:44:36 +0530

    transfer

--- endpoints.py
+++ endpoints.py
@@ -52,6 +52,47 @@ def conference():
     return Response(str(r), mimetype='text/xml')
 
 
+@app.route('/transfer', methods=['POST', 'GET'])
+def transfer():
+    r = plivoxml.Response()
+    getdigits_action_url = url_for('transfer_action', _external=True)
+    getDigits = plivoxml.GetDigits(action=getdigits_action_url,
+            method='GET', timeout=7, numDigits=1,
+            retries=1, redirect='false')
+    getDigits.addSpeak("Press 1 to transfer this call")
+    r.add(getDigits)
+    params = {
+        'length' : "10" # Time to wait in seconds
+    }
+    r.addWait(**params)
+    print (r.to_xml())
+    return Response(str(response), mimetype='text/xml')
+
+
+
+@app.route('/transfer_action/', methods=['POST', 'GET'])
+def transfer_action():
+    digit = request.args.get('Digits')
+    call_uuid = request.args.get('CallUUID')
+    auth_id = "MAMZAYOTJJMDM3NDQ2OT"
+    auth_token = "ODE1ZmJkNzI3MzIwMmNmMDBiMDFiNjkxMDhlMjZj"
+    print ("Call UUID is : %s") % (call_uuid)
+    print ("Digit pressed is : %s")  % (digit)
+    p = plivo.RestAPI(auth_id, auth_token)
+    if digit == "1":
+        params = {
+            'call_uuid' : call_uuid, # ID of the call
+            'aleg_url' : "http://plivo-flask-training.herokuapp.com/forward", # URL to transfer for aleg
+            'aleg_method' : "GET" # ethod to invoke the aleg_url
+        }
+        response = p.transfer_call(params)
+    else :
+        print ("Wrong Input")
+        print (str(response))
+        return Response(str(response), mimetype='text/plain')
+
+
+
 @app.route('/')
 def index():
     return 'Hello, World'
